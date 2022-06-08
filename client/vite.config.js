f96b2a1fe1c2eaec017a4470b25a33352d53444d
dominggo1999 <arnolddominggo19@gmail.com>
2022-06-08 18:43:28 +0700

    add more scripts

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,29 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: ['babel-plugin-macros', 'babel-plugin-styled-components'],
-      },
-    }),
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001/api',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: ['babel-plugin-macros', 'babel-plugin-styled-components'],
+        },
+      }),
+    ],
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3001/api',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
+  };
 });
